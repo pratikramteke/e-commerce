e12114b84bf6aef83135d8e4a445fec88485c803
pratikramteke <pratikramteke222@gmail.com>
2023-07-07 16:47:39 +0530

    home page added

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,9 @@
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import forms from "@tailwindcss/forms";
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
-
-export default App
