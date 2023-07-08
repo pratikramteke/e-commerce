29ed6c60568b9722c942e5e96b2799ab7eb09eed
pratikramteke <pratikramteke222@gmail.com>
2023-07-08 11:56:23 +0530

    Routing done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
+
 import "./App.css";
 import Home from "./pages/Home";
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 export default function App() {
   return (
     <div className="App">
-      <Home />
+     <RouterProvider router={router} />
     </div>
   );
 }
