5896558f1f592cffbd2e962f015335c55b1e2c88
pratikramteke <pratikramteke222@gmail.com>
2023-07-15 18:33:31 +0530

    product details page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
+import { Counter } from "./features/counter/Counter"
+import "./App.css"
 import Home from "./pages/Home"
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
-import CartPage from "./pages/CartPage"
-import Checkout from "./pages/Checkout"
-import ProductDetailPage from "./pages/ProductDetailPage"
+
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
 } from "react-router-dom"
-
+import Cart from "./features/cart/Cart"
+import CartPage from "./pages/CartPage"
+import Checkout from "./pages/Checkout"
+import ProductDetailPage from "./pages/ProductDetailPage"
 const router = createBrowserRouter([
   {
     path: "/",
@@ -33,15 +36,17 @@ const router = createBrowserRouter([
     element: <Checkout></Checkout>,
   },
   {
-    path: "/product-detail",
+    path: "/product-detail/:id",
     element: <ProductDetailPage></ProductDetailPage>,
   },
 ])
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <RouterProvider router={router} />
     </div>
   )
 }
+
+export default App
