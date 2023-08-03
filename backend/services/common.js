24051c7494339dd6613c2e511b6ceb661b0f3948
pratikramteke <pratikramteke222@gmail.com>
2023-08-03 21:04:26 +0530

    connected auth with react with cookie

--- backend/services/common.js
+++ backend/services/common.js
@@ -7,3 +7,14 @@ exports.isAuth = (req, res, done) => {
 exports.sanitizeUser = (user) => {
   return { id: user.id, role: user.role }
 }
+
+exports.cookieExtractor = function (req) {
+  let token = null
+  if (req && req.cookies) {
+    token = req.cookies["jwt"]
+  }
+  //TODO : this is temporary token for testing without cookie
+  token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NTgyNDVkMGQ3ZGU3YzMxMTQ5OWFlZSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjgzNDk4MDc3fQ.hAh6nIrULH0mOk5RXbu_g_9tGCmsg2gkXgIVw02Dczg"
+  return token
+}
